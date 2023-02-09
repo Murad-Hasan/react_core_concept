13f97b35505da85be6227c38b362fd5e6926f9a1
Murad-Hasan <muradhasan.7845@gmail.com>
2023-02-09 11:15:06 +0600

    use of child component

--- react_core/src/components/app.jsx
+++ react_core/src/components/app.jsx
@@ -2,14 +2,35 @@ import { Component } from "react";
 import Bio from "./profile/bio";
 import Profile from "./profile/index";
 import Skills from "./profile/skill";
+
+class Child extends Component {
+  render() {
+    this.props.func(this);
+    return <h1>I am child</h1>;
+  }
+}
+
+const ChildComponent = (props) => {
+  return(
+    <div>
+    <h1>I am child</h1>
+    <p>I don't know what to do</p>
+    {props.children}
+  </div>
+  )
+};
+
 class App extends Component {
+  getContext(context) {
+    console.log(context);
+  }
   render() {
     return (
       <div>
         <Profile />
         <Bio name='Hasan' title='junior developer' />
         <p>Mr.X</p>
-        <Skills 
+        <Skills
           heading='Skills'
           skillA='vue'
           skillB='svelte'
@@ -17,13 +38,17 @@ class App extends Component {
           skillD='React'
         />
         <p>Mr.Y</p>
-        <Skills 
+        <Skills
           heading='Skills'
           skillA='Kotlin'
           skillB='Java'
           skillC='Go'
           skillD='C++'
         />
+        {/* <Child func={this.getContext} /> */}
+        <ChildComponent>
+          <p>I am child from parents</p>
+        </ChildComponent>
       </div>
     );
   }
