2d7f0454d9f61a4a0e1df11377e95ab35dadf26d
Murad-Hasan <muradhasan.7845@gmail.com>
2023-02-11 12:31:54 +0600

    timer complete

--- react_core/src/components/simpleTimer/index.jsx
+++ react_core/src/components/simpleTimer/index.jsx
@@ -4,6 +4,9 @@ class Timer extends Component {
   state = {
     count: 0,
   };
+
+  intervalId = null;
+
   incrementCount = () => {
     this.setState({ count: this.state.count + 1 });
   };
@@ -12,25 +15,70 @@ class Timer extends Component {
       this.setState({ count: this.state.count - 1 });
     }
   };
+  startTimer = () => {
+    if (this.state.count > 0 && !this.intervalId) {
+      this.intervalId = setInterval(() => {
+        this.setState({ count: this.state.count - 1 }, () => {
+          if (this.state.count === 0) {
+            alert("Time is up");
+            clearInterval(this.intervalId);
+            this.intervalId = null;
+          }
+        });
+      }, 1000);
+    }
+  };
+  stopTimer = () => {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
+  };
+  resetTimer = () => {
+    this.setState({ count: 0 });
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+  };
   render() {
     return (
       <div className='container mx-auto text-center'>
-        <h1>Timer</h1>
-        <div className='border p-5'>
+        <h1 className="text-2xl font-bold py-2">Simple Timer</h1>
+        <div className='border p-5 border-green-500'>
           <div className='timer__section'>
-            <button className='px-2 border border-gray-400' onClick={this.decrementCount}>
+            <button
+              className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold active:scale-90 rounded hover:text-white px-2 border border-blue-500 hover:border-transparent'
+              onClick={this.decrementCount}
+            >
               -
             </button>
             <span className='px-2'>{this.state.count}</span>
-            <button className='px-2 border border-gray-400' onClick={this.incrementCount}>
+            <button
+              className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold active:scale-90 rounded hover:text-white px-2 border border-blue-500 hover:border-transparent'
+              onClick={this.incrementCount}
+            >
               {" "}
               +{" "}
             </button>
           </div>
           <div className='stop__watch py-3'>
-            <button className='px-2 ml-2 border border-gray-400'>Start</button>
-            <button className='px-2 ml-2 border border-gray-400'>Stop</button>
-            <button className='px-2 ml-2 border border-gray-400'>Reset</button>
+            <button
+              onClick={this.startTimer}
+              className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 active:scale-90 hover:border-transparent rounded ml-2'
+            >
+              Start
+            </button>
+            <button
+              onClick={this.stopTimer}
+              className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 active:scale-90 hover:border-transparent rounded ml-2'
+            >
+              Stop
+            </button>
+            <button
+              onClick={this.resetTimer}
+              className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 active:scale-90 hover:border-transparent rounded ml-2'
+            >
+              Reset
+            </button>
           </div>
         </div>
       </div>
