bdbd7b244cd02d2eb0dd621019b646e8bfa4f5ff
Murad-Hasan <muradhasan.7845@gmail.com>
2023-02-11 15:42:06 +0600

    add timer component and tailwindcss

--- react_core/src/components/simpleTimer/index.jsx
+++ react_core/src/components/simpleTimer/index.jsx
@@ -5,16 +5,22 @@ import TimerCount from "./timerCount";
 class Timer extends Component {
   state = {
     count: 0,
+    minute: 0,
   };
 
   intervalId = null;
 
   incrementCount = () => {
     this.setState({ count: this.state.count + 1 });
+    if (this.state.count === 59) {
+      this.setState({ count: 0, minute: this.state.minute + 1 });
+    }
   };
   decrementCount = () => {
     if (this.state.count > 0) {
       this.setState({ count: this.state.count - 1 });
+    } else if (this.state.count === 0 && this.state.minute > 0) {
+      this.setState({ count: 59, minute: this.state.minute - 1 });
     }
   };
   startTimer = () => {
@@ -30,6 +36,7 @@ class Timer extends Component {
       }, 1000);
     }
   };
+
   stopTimer = () => {
     if (this.intervalId) {
       clearInterval(this.intervalId);
@@ -54,7 +61,7 @@ class Timer extends Component {
               {" "}
               -{" "}
             </Button>
-            <TimerCount style="p-2 text-2xl font-bold">{this.state.count}s</TimerCount>
+            <TimerCount style="p-2 text-2xl font-bold">{this.state.minute}m:{this.state.count}s</TimerCount>
             <Button
               btnStyle='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold active:scale-90 rounded hover:text-white px-2 border border-blue-500 hover:border-transparent'
               event={this.incrementCount}
